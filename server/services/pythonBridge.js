a3663ab4edb86dad0af673f5f2fbda846816a5eb
Tanishq Singh <tanishq.24singh@gmail.com>
2026-01-27 23:00:54 +0530

    Minor Fixes

--- server/services/pythonBridge.js
+++ server/services/pythonBridge.js
@@ -1,5 +1,7 @@
 // Calls the Python Fabric Data Agent bridge (Flask) instead of hitting Fabric directly.
 // Expects the Python service to run locally at DATA_AGENT_PY_URL (default http://localhost:5050/ask).
+import fetch from 'node-fetch';
+
 const PY_URL = process.env.DATA_AGENT_PY_URL || 'http://localhost:5050/ask';
 
 export async function askDataAgentViaPython(question, language = 'en') {
